64649af4562ebd977f7c1824b9f53b654e8351f0
Shloken <ahainer123@gmail.com>
2018-05-15 15:40:52 -0400

    Added Component Files

---
+++ src/components/logger/EzlogLogger.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './EzlogLogger.css';
+
+class EzlogLogger extends Component {
+
+    render() {
+        return (
+            <div className='logger'>
+                
+            </div>
+        )
+    }
+}
+
+export default EzlogLogger;
---
+++ src/components/widget/EzlogWidget.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './EzlogWidget.css';
+
+class EzlogWidget extends Component {
+
+    render() {
+        return (
+            <div className='widget'>
+                
+            </div>
+        )
+    }
+}
+
+export default EzlogWidget;
