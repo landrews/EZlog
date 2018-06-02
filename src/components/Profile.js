ab5c9b5133647a0deaeee571559bb120a45ae426
lotekmedia <me@johnmann.org>
2018-06-02 19:50:47 -0400

    profile

---
+++ src/components/Profile.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AuthUserContext from './AuthUserContext'
+import { firebase } from '../firebase'
+
+class ProfileControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.saveProfileClick = this.saveProfileClick.bind(this);
+        this.state = { currentProfile: null }
+    }
+
+    componentDidMount() {
+        this.getProfile()
+    }
+
+    updateInputValue = (item) => {
+        this.setState({ currentProfile: {FirstName: item.value} })
+    }
+
+    getProfile = () => {
+        var self = this;
+        var ref = firebase.db.ref('profiles').child(this.props.uid)
+        ref.on('value', function (results) {
+            var profile = results.val()
+            self.setState({ currentProfile: profile })
+        });
+    }
+
+    renderProfile = (profile) => {
+        if (profile) {
+            return (
+                <div>
+                    <input name="FirstName" key={profile.FirstName} onChange={evt => this.updateInputValue(evt.target)} value={profile.FirstName} />
+                    <input name="LastName" key={profile.LastName} value={profile.LastName} />
+                    <input name="Email" key={profile.Email} value={profile.Email} />
+                </div>
+            )
+        }
+    }
+
+    saveProfileClick(uid) {
+        var myProfile = firebase.db.ref('profiles')
+        var newProfile = { DisplayName: 'John Mann' }
+        myProfile.child(uid).update(newProfile)
+    }
+
+    render() {
+        var self = this;
+        return (
+            <AuthUserContext.Consumer>
+                {authUser =>
+                    authUser != null ?
+                        <span>
+                            {this.state.currentProfile != null ?
+                                <div>
+                                    {this.renderProfile(this.state.currentProfile)}
+                                </div> :
+                                <div>here</div>
+                            }
+                            <SaveProfileButton onClick={evt => this.saveProfileClick(authUser.uid)} />
+                        </span> : <div>Please Login</div>
+                }
+            </AuthUserContext.Consumer>
+        );
+    }
+}
+
+export default ProfileControl
+
+function SaveProfileButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Save Settings
+      </button>
+    )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import Layout from '../components/Layout'
 import React from 'react'
 import { firebase } from '../firebase'
+import AuthUserContext from '../components/AuthUserContext'
+import ProfileControl from '../components/Profile'
 
 class About extends React.Component {
     constructor(props) {
@@ -8,43 +10,17 @@ class About extends React.Component {
         this.state = { profiles: null }
     }
 
-    componentDidMount() {
-        this.getProfiles()
-    }
-
-    renderProfiles = (profiles) => {
-        var self = this
-        if (profiles) {
-            var displayMessage = profiles.map(function (item) {
-                return <div key={item.key}>{item.profile.displayName}</div>
-            })
-            return displayMessage
-        }
-    }
-
-    getProfiles = () => {
-        var self = this;
-        var ref = firebase.db.ref('profiles').orderByChild('displayName').equalTo('Test Key')
-        ref.on('value', function (results) {
-            let allProfiles = []
-            var profiles = results.val()
-            // iterate through results coming from database call; messages
-            for (var item in profiles) {
-                allProfiles.push({ key: item, profile: profiles[item] })
-            }
-            self.setState({ profiles: allProfiles })
-        });
-    }
-
     render() {
         return (
             <Layout>
-                    <p>This is the about page</p>
-                    {this.state.profiles && (
-                        <div>
-                            {this.renderProfiles(this.state.profiles)}
-                        </div>
-                    )}
+                <p>This is the about page!</p>
+                <AuthUserContext.Consumer>
+                    {authUser =>
+                        authUser != null ?
+                        <ProfileControl uid={authUser.uid} /> :
+                        <div>-</div>
+                    }
+                </AuthUserContext.Consumer>
             </Layout>
         )
     }
